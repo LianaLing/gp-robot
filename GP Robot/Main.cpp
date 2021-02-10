0e7736a52d1dfd407216bcf695ccb2d79319c479
DannyPhang <dannyphang64@gmail.com>
2021-02-11 04:19:34 +0800

    Update Main.cpp

--- GP Robot/Main.cpp
+++ GP Robot/Main.cpp
@@ -5,17 +5,18 @@
 
 #pragma comment (lib, "OpenGL32.lib")
 
-#define WINDOW_TITLE "Practical 1"
+#define WINDOW_TITLE "IRON-MAN MK47"
 #define CW_USEDEFAULT 10
-#define WIDTH 800
-#define HEIGHT 800
+#define WIDTH 900.0
+#define HEIGHT 900.0
+#define DEPTH 800.0
 #define M_PI 3.2
 
-int qNo = 1;
-float xConvertion(float x) {
+int qNo = 3;
+float xC(float x) {
 	float xCoor, width = WIDTH / 2;
 	if (x < width) {
-		xCoor = 0 - (x / width);
+		xCoor = (x / width) - 1;
 	}
 	else if (x > width) {
 		xCoor = (x - width) / width;
@@ -26,10 +27,10 @@ float xConvertion(float x) {
 	return xCoor;
 }
 
-float yConvertion(float y) {
+float yC(float y) {
 	float yCoor, height = HEIGHT / 2;
 	if (y < height) {
-		yCoor = y / height;
+		yCoor = 1 - (y / height);
 	}
 	else if (y > height) {
 		yCoor = 0 - ((y - height) / height);
@@ -40,11 +41,25 @@ float yConvertion(float y) {
 	return yCoor;
 }
 
-float xPixel(float x) {
+float zC(float z) {
+	float zCoor, depth = DEPTH / 2;
+	/*if (z < 0) {
+		zCoor = (z / depth);
+	}
+	else if (z > depth) {
+		zCoor = (z - depth) / depth;
+	}
+	else if (z == depth) {
+		zCoor = 0;
+	}*/
+	return (z / depth);
+}
+
+float xP(float x) {
 	return (x / (WIDTH / 2));
 }
 
-float yPixel(float y) {
+float yP(float y) {
 	return (y / (HEIGHT / 2));
 }
 
@@ -144,13 +159,45 @@ bool initPixelFormat(HDC hdc)
 void drawCircle(float x1, float y1, float x, float y, float min, float max) {
 	for (float i = min; i < max; i++)
 	{
-		float x2 = xConvertion(x1) + cos(i * M_PI / 180.f) * xPixel(x);
-		float y2 = yConvertion(y1) + sin(i * M_PI / 180.f) * yPixel(y);
+		float x2 = xC(x1) + cos(i * M_PI / 180.f) * xP(x);
+		float y2 = yC(y1) + sin(i * M_PI / 180.f) * yP(y);
 		glVertex2f(x2, y2);
 	}
 }
 
-void test() {
+void drawSphere(float r, int xaxis, int yaxis, float zaxis, float xShape, float yShape) {
+	float i, j, lats = 30, longs = 30;
+	float x2 = xC(xaxis), y2 = yC(yaxis), z2 = zC(zaxis);
+
+	for (i = 1; i <= xShape; i++) {
+		float lat0 = M_PI * (-0.5 + (i - 1) / lats);
+		float z0 = sin(lat0);
+		float zr0 = cos(lat0);
+
+		float lat1 = M_PI * (-0.5 + i / lats);
+		float z1 = sin(lat1);
+		float zr1 = cos(lat1);
+
+		glBegin(GL_QUAD_STRIP);
+		for (j = 0; j <= yShape; j++) {
+			float lng = 2 * M_PI * (j - 1.0) / longs;
+			float x = cos(lng);
+			float y = sin(lng);
+
+			glColor3f(1, 1, 1);
+			glNormal3f(x2 + x * zr0, y2 + y * zr0, z2 + z0);
+
+			glVertex3f(x2 + r * x * zr0, y2 + r * y * zr0, z2 + r * z0);
+			glColor3f(1, 0, 0);
+			glNormal3f(x2 + x * zr1, y2 + y * zr1, z2 + z1);
+
+			glVertex3f(x2 + r * x * zr1, y2 + r * y * zr1, z2 + r * z1);
+		}
+		glEnd();
+	}
+}
+
+void test1() {
 	glClearColor(0.0745, 0.109803, 0.129411, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
@@ -158,58 +205,58 @@ void test() {
 	float angle;
 
 	// head
-	x1 = 0, y1 = 0;
+	x1 = 450, y1 = 450;
 	glBegin(GL_POLYGON);
 	glColor3f(0.97647, 0.709804, 0.152941);
-	drawCircle(x1, y1, 0.4, 0.4, 0, 371);
+	drawCircle(x1, y1, 180, 180, 0, 371);
 
 	glEnd();
 
 	// smile 1
-	x1 = 0, y1 = -0.05;
+	x1 = 450, y1 = 472.5;
 	glLineWidth(2);
 	glBegin(GL_POLYGON);
 	glColor3f(0.45098, 0.28627, 0.054902);
-	drawCircle(x1, y1, 0.2, 0.15, 200, 330);
+	drawCircle(x1, y1, 90, 67.5, 200, 330);
 
 	glEnd();
 
 	// smile 1 cover
-	x1 = 0, y1 = -0.05;
+	x1 = 450, y1 = 472.5;
 	glLineWidth(2);
 	glBegin(GL_POLYGON);
 	glColor3f(0.97647, 0.709804, 0.152941);
-	drawCircle(x1, y1, 0.2, 0.13, 200, 330);
+	drawCircle(x1, y1, 90, 58.5, 200, 330);
 
 	glEnd();
 
 	// eye 1
-	x1 = -0.14, y1 = 0.1;
+	x1 = 387, y1 = 405;
 	glBegin(GL_POLYGON);
 	glColor3f(0.45098, 0.28627, 0.054902);
-	drawCircle(x1, y1, 0.05, 0.05, 0, 371);
+	drawCircle(x1, y1, 22.5, 22.5, 0, 371);
 
 	glEnd();
 
-	x1 = 0.14, y1 = 0.1;
+	x1 = 513, y1 = 405;
 	glBegin(GL_POLYGON);
 	glColor3f(0.45098, 0.28627, 0.054902);
-	drawCircle(x1, y1, 0.05, 0.05, 0, 371);
+	drawCircle(x1, y1, 22.5, 22.5, 0, 371);
 
 	glEnd();
 
 	// eye 2
-	x1 = -0.14, y1 = 0.12;
+	x1 = 387, y1 = 396;
 	glBegin(GL_POLYGON);
 	glColor3f(1, 1, 1);
-	drawCircle(x1, y1, 0.02, 0.02, 0, 371);
+	drawCircle(x1, y1, 9, 9, 0, 371);
 
 	glEnd();
 
-	x1 = 0.14, y1 = 0.12;
+	x1 = 513, y1 = 396;
 	glBegin(GL_POLYGON);
 	glColor3f(1, 1, 1);
-	drawCircle(x1, y1, 0.02, 0.02, 0, 371);
+	drawCircle(x1, y1, 9, 9, 0, 371);
 
 	glEnd();
 
@@ -224,18 +271,24 @@ void test() {
 	glEnd();
 
 	// tear 2
-	x1 = 0.14, y1 = -0.15;
+	x1 = 513, y1 = 517.5;
 	glBegin(GL_POLYGON);
 	glColor3f(0.48627, 0.839215, 0.890196);
-	drawCircle(x1, y1, 0.11, 0.13, 177, 354);
+	drawCircle(x1, y1, 49.5, 58.5, 177, 354);
 
 	glEnd();
+}
+
+void test2() {
+	//glLoadIdentity();
+	glRotatef(1, 1, 1, 0);
+	drawSphere(0.3, 400, 400, 0, 30, 30);
 }
 
 void pahangFlag() {
 
 	glClearColor(255, 255, 255, 255);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// flag shape
 	glLineWidth(1);
@@ -247,12 +300,14 @@ void pahangFlag() {
 	glVertex2f(0.8, -0.5);
 	glVertex2f(-0.8, -0.5);*/
 
-	glVertex2f(xConvertion(320), yConvertion(200));
-	glVertex2f(xConvertion(720), yConvertion(200));
-	glVertex2f(xConvertion(720), yConvertion(600));
-	glVertex2f(xConvertion(320), yConvertion(600));
+	// white
+	glVertex2f(xC(90), yC(200));
+	glVertex2f(xC(810), yC(200));
+	glVertex2f(xC(810), yC(600));
+	glVertex2f(xC(90), yC(600));
 	glEnd();
 
+	// black
 	glBegin(GL_POLYGON);
 	glColor3f(0, 0, 0);
 	glVertex2f(-0.8, 0);
@@ -285,7 +340,7 @@ void japanFlag() {
 	float angle;
 	double radius = 0.3;
 
-	x1 = 400.0, y1 = 400.0;
+	x1 = 450.0, y1 = 450.0;
 	glColor3f(1, 0, 0);
 
 	glBegin(GL_TRIANGLE_FAN);
@@ -294,512 +349,31 @@ void japanFlag() {
 	glEnd();
 }
 
-void extra() {
-
-	float x1 = 0, y1 = 0, x2 = 0, y2 = 0;
-
-	glClearColor(0, 0, 0, 0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glLineWidth(3);
-
-	// background
-	glBegin(GL_POLYGON);
-	glColor3f(0, 0, 0.5);
-	glVertex2f(-1, 1);
-	glVertex2f(1, 1);
-	glColor3f(1, 1, 1);
-	glVertex2f(1, -1);
-	glVertex2f(-1, -1);
-
-	glEnd();
-
-	// head
-	float radius = 0.36;
-	x1 = 0, y1 = -0.027;
-
-	glBegin(GL_POLYGON);
-	glColor3f(0, 0.5921, 0.890196);
-	for (GLfloat i = -47; i < 225; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.347; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.36;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// head border 
-	x1 = 0, y1 = -0.027;
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = -48; i < 227; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.347; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.36;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// head2
-	x1 = 0.0, y1 = -0.07;
-	glBegin(GL_POLYGON);
-	glColor3f(1, 1, 1);
-	for (GLfloat i = -43.3; i < 221; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.3; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.32;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-
-	// mouth
-	x1 = 0.0, y1 = -0.05;
-	glBegin(GL_POLYGON);
-	glColor3f(0.890196, 0.196088, 0.047058);
-	for (GLfloat i = 175; i < 355; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.24; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.23;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// mouth 3 version 2
-	x1 = 0.0, y1 = -0.3;
-	glBegin(GL_POLYGON);
-	glColor3f(0.890196, 0.494117, 0.30196);
-	for (GLfloat i = 27; i < 150; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.17; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.15;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	x1 = 0.0, y1 = -0.15;
-	glBegin(GL_POLYGON);
-	glColor3f(0.890196, 0.494117, 0.30196);
-	for (GLfloat i = 215; i < 320; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.185; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.13;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// mouth 3 border
-	x1 = 0.0, y1 = -0.3;
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 27; i < 150; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.17; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.15;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// mouth border
-	x1 = 0.0, y1 = -0.05;
-	glBegin(GL_LINE_LOOP);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 176; i < 355; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.24; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.23;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// eyes 1 
-	x1 = -0.05, y1 = 0.24;
-	glBegin(GL_POLYGON);
-	glColor3f(1, 1, 1);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.05; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.08;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// eyes 2 
-	x1 = 0.05, y1 = 0.24;
-	glBegin(GL_POLYGON);
-	glColor3f(1, 1, 1);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.05; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.08;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// eyes 1 border
-	x1 = -0.05, y1 = 0.24;
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-	//glVertex2f(x1, y1);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.05; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.08;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// eyes 2 border
-	x1 = 0.05, y1 = 0.24;
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.05; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.08;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// eye dots 1
-
-	x1 = -0.03, y1 = 0.24;
-	glBegin(GL_POLYGON);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.015; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.03;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// eye dots 1.5
-
-	x1 = -0.027, y1 = 0.24;
-	glBegin(GL_POLYGON);
-	glColor3f(1, 1, 1);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.005; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.01;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// eye dots 2
-
-	x1 = 0.03, y1 = 0.24;
-	glBegin(GL_POLYGON);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.015; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.03;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// eye dots 2.5
-
-	x1 = 0.027, y1 = 0.24;
-	glBegin(GL_POLYGON);
-	glColor3f(1, 1, 1);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.005; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.01;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// nose 
-	radius = 0.04;
-
-	x1 = 0.0, y1 = 0.14;
-	glColor3f(1, 0, 0);
-
-	glBegin(GL_POLYGON);
-	glVertex2f(x1, y1);
-
-	drawCircle(x1, y1, 0.04, 0.04, 0, 360);
-	glEnd();
-
-	// nose border
-
-	x1 = 0, y1 = 0.14;
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.04; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.04;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// nose 2
-	radius = 0.01;
-
-	x1 = -0.01, y1 = 0.15;
-	glColor3f(1, 1, 1);
-
-	glBegin(GL_POLYGON);
-	glVertex2f(x1, y1);
-
-	drawCircle(x1, y1, radius, radius, 0, 360);
-
-	glEnd();
-
-	// nose line
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(0, 0.1);
-	glVertex2f(0, -0.05);
-
-	glEnd();
-
-	// belt
-	glBegin(GL_POLYGON);
-	glColor3f(0.82745, 0.211766, 0.090196);
-
-	glVertex2f(-0.22, -0.3);
-	glVertex2f(0.22, -0.3);
-	glVertex2f(0.22, -0.34);
-	glVertex2f(-0.22, -0.34);
-
-	glEnd();
-
-	// belt border
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(-0.22, -0.3);
-	glVertex2f(0.22, -0.3);
-	glVertex2f(0.22, -0.34);
-	glVertex2f(-0.22, -0.34);
-
-	glEnd();
-
-	//belt left
-	x1 = -0.22, y1 = -0.32;
-	glBegin(GL_POLYGON);
-	glColor3f(0.82745, 0.211766, 0.090196);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.02; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.02;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 64; i < 300; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.02; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.02;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	//belt right
-	x1 = 0.22, y1 = -0.32;
-	glBegin(GL_POLYGON);
-	glColor3f(0.82745, 0.211766, 0.090196);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.02; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.02;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.02; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.02;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0.82745, 0.211766, 0.090196);
-	for (GLfloat i = 130; i < 220; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.02; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.02;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// bell
-	x1 = 0, y1 = -0.36;
-	glBegin(GL_POLYGON);
-	glColor3f(0.894117, 0.866667, 0.007843);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.04; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.04;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// bell border
-	x1 = 0, y1 = -0.36;
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.04; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.04;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// bell 2
-	glBegin(GL_POLYGON);
-	glColor3f(0.894117, 0.866667, 0.007843);
-
-	glVertex2f(-0.045, -0.35);
-	glVertex2f(0.04, -0.33);
-	glVertex2f(0.04, -0.34);
-	glVertex2f(-0.04, -0.36);
-
-	glEnd();
-
-
-	// bell 2 border
-	glLineWidth(1.5);
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(-0.05, -0.35);
-	glVertex2f(0.04, -0.33);
-	glVertex2f(0.04, -0.34);
-	glVertex2f(-0.05, -0.36);
-
-	glEnd();
-
-	// bell 3
-	x1 = 0.0025, y1 = -0.37;
-	glBegin(GL_POLYGON);
-	glColor3f(0, 0, 0);
-	for (GLfloat i = 0; i < 360; i++)
-	{
-		float x2 = x1 + cos(i * M_PI / 180.f) * 0.015; // keep the axes radius same
-		float y2 = y1 + sin(i * M_PI / 180.f) * 0.015;
-		glVertex2f(x2, y2);
-	}
-
-	glEnd();
-
-	// bell line
-	glLineWidth(3);
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(0.003, -0.37);
-	glVertex2f(0.003, -0.4);
-
-	glEnd();
-
-	// beard left 
-	glLineWidth(5);
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(-0.21, 0.11);
-	glVertex2f(-0.05, 0.07);
-
-	glEnd();
-
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(-0.22, 0.05);
-	glVertex2f(-0.05, 0.05);
-
-	glEnd();
-
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(-0.22, -0.01);
-	glVertex2f(-0.05, 0.03);
-
-	glEnd();
-
-	// beard right 
-	glLineWidth(5);
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(0.21, 0.11);
-	glVertex2f(0.05, 0.07);
-
-	glEnd();
-
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(0.22, 0.05);
-	glVertex2f(0.05, 0.05);
-
-	glEnd();
-
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0, 0, 0);
-
-	glVertex2f(0.22, -0.01);
-	glVertex2f(0.05, 0.03);
-
-	glEnd();
-}
-
 void display()
 {
+	glClearColor(0, 0, 0, 0);
+	//glEnable(GL_DEPTH_TEST);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	switch (qNo) {
 	case 1:
 		pahangFlag();
 		break;
 	case 2:
-		extra();
+		//extra();
 		break;
 	case 3:
-		test();
+		test1();
 		break;
 	case 4:
 		japanFlag();
 		break;
+	case 5:
+		test2();
+		break;
 	default:
 		pahangFlag();
+		break;
 	}
 
 }
